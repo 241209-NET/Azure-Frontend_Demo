12dab8fc3bfc1cdc9f55a280f1bf8c47e2fbc14d
Kung Lo <kungyang.lo@revature.com>
2025-01-15 08:59:13 -0500

    changed some names around

--- react-demo/src/components/PokemonSearch.jsx
+++ react-demo/src/components/PokemonSearch.jsx
@@ -1,65 +1,66 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-function PokemonSearch() {
+function PetSearch() {
 
     //Setting up state variables
-    const [pokemonToFind, setPokemonToFind] = useState(1); //By default, it loads Bulbasaur 
-    const [pokemonData, setPokemonData] = useState(null); //By default, before the user searches this is null
+    const [petToFind, setPetToFind] = useState(1); //By default, it loads Bulbasaur 
+    const [petData, setPetData] = useState(null); //By default, before the user searches this is null
 
     //Setting up our useEffect to fetch pokemon data when component mounts OR when it re-renders
     useEffect(() => {
         
         //This is a function that I will call to send that GET request to the pokeAPI
-        const fetchPokemonData = async () => {
+        const fetchPetData = async () => {
             try{
                 //First we try to get a response from pokeAPI
-                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonToFind}`); 
+                const response = await axios.get(`https://petsdb.azurewebsites.net/api/pet/id/${petToFind}`); 
                 
-                setPokemonData({
+                setPetData({
+                    id: response.data.id,
                     name: response.data.name,
-                    sprite: response.data.sprites.front_default,
-                    types: response.data.types.map((typeInfo) => typeInfo.type.name).join(', '),
+                    type: response.data.type,
+                    dob: response.data.birthday
                 });
 
             } catch (error) {
-                console.error('Error fetching Pokemon data:', error)
-                setPokemonData(null);
+                console.error('Error fetching Pet data:', error)
+                setPetData(null);
             }
         };
 
         //Here we just call the function
-        fetchPokemonData();
+        fetchPetData();
 
-    }, [pokemonToFind]); // UseEffect exepcts a dependency array as a second argument.
+    }, [petToFind]); // UseEffect exepcts a dependency array as a second argument.
     //Even if you have none, omitting this can result in an infinite loop. 
 
     const handleInputChange = (event) => {
-        setPokemonToFind(event.target.value);
+        setPetToFind(event.target.value);
     }
 
 
   return (
     <div>
-        <h2>Pokemon Search</h2>
+        <h2>Pet Search</h2>
         <input 
             type="text" 
-            value={pokemonToFind}
+            value={petToFind}
             onChange={handleInputChange}
-            placeholder='Enter pokemon to search for'
+            placeholder='Enter pet id to search for'
         />
 
         {/* Conditionally render the pokeData if any has ben returned */}
         {
-            pokemonData ? (
+            petData ? (
                 <div>
-                    <h3>{pokemonData.name}</h3>
-                    <img src={pokemonData.sprite} alt={pokemonData.name} />
-                    <p>Type(s): {pokemonData.types}</p>
+                    <h3>{petData.name}</h3>
+                    <p>Type: {petData.type}</p>
+                    <p>Birthday: {petData.dob}</p>
                 </div>
             ) : (
                 <div>
-                    <p>Loading Pokemon data...</p>
+                    <p>Loading Pet data...</p>
                 </div>
             )
         }
@@ -70,4 +71,4 @@ function PokemonSearch() {
   )
 }
 
-export default PokemonSearch
+export default PetSearch
